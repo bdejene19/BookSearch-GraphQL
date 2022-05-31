9514194595f5b0690709f9a830a811311b8fe68a
Bemnet Dejene <bdejene19@gmail.com>
2022-05-31 15:31:48 -0400

    wrote typeDefs for object types, Queries, and Mutations

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    authors: [String]!
+    description: String!
+    bookId: String!
+    title: String!
+
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    me: User!
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookContent: Book!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
